65f3bad8a90db8355f436a141f22ba968392546b
Disargee18 <aboy.paqs@gmail.com>
2025-07-15 02:00:45 +0800

    [+] first commit

---
+++ src/services/api.js
@@ -0,0 +1,14 @@
+const API_KEY = "0fc7d7e640d84bb718de7af2111304bc";
+const BASE_URL = "https://api.themoviedb.org/3"
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const data = await response.json()
+    return data.results
+};
+
+export const searchMovies = async (query) => {
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
+    const data = await response.json()
+    return data.results
+};
